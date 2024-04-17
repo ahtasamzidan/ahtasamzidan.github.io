310a1bc8cd0aa044d269f5116fa65730652d1a30
Ahtasam Zidan <164373344+ahtasamzidan@users.noreply.github.com>
2024-04-17 22:24:20 +0800

    Update script.js

--- files/script.js
+++ files/script.js
@@ -6,6 +6,15 @@ var typed = new Typed(".text", {
     loop:true
 });
 
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+menuIcon.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+navbg.classList.toggle('active');
+});
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
