fd6c2b85183cdb0d7bd5ec8d3f0cca2477c01f2f
Ahtasam Zidan <164373344+ahtasamzidan@users.noreply.github.com>
2024-04-17 22:23:58 +0800

    script.js

--- files/script.js
+++ files/script.js
@@ -1,12 +1,3 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-const navbg = document.querySelector('.nav-bg');
-menuIcon.addEventListener('click', () => {
-menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
-navbg.classList.toggle('active');
-});
-
 var typed = new Typed(".text", {
     strings: ["Graphics Design" , "Digital Marketing" , "Web Development"],
     typeSpeed:100,
